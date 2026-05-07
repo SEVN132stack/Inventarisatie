576bff69379573f00a67ac4f74e7066c33f53c35
SEVN132stack <troy.janssens.it@gmail.com>
2026-05-07 19:47:08 +0200

    dd

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  // Sla type-checking over tijdens Docker build (tsc loopt al apart)
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-}
-
-module.exports = nextConfig
